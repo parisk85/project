6b4a46e11b502804f421a43505389728c02f41e2
parisk88 <parisk85@gmail.com>
2022-10-10 22:33:19 +0300

    Initial commit

---
+++ src/test/java/gr/parisk85/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.parisk85.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
